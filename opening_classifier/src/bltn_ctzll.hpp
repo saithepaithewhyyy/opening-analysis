6c49118d6f92fd66d0a5e75b43402cb2f94c2b84
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-03-28 10:33:34 +0530

    movegen doesnt consider negative direction moves because I used MSB

--- opening_classifier/src/bltn_ctzll.hpp
+++ opening_classifier/src/bltn_ctzll.hpp
@@ -8,9 +8,20 @@
         _BitScanForward64(&index, x);
         return index;
     }
+    inline int clzll(unsigned long long x) {
+        if (x == 0) return -1;  // safety
+        unsigned long index;
+        _BitScanReverse64(&index, x);
+        return index;
+    }
+
 #else
     inline int ctzll(unsigned long long x) {
         if (x == 0) return -1;  // safety
         return __builtin_ctzll(x);
+    }
+    inline int clzll(unsigned long long x){
+        if (x==0) return -1;
+        return __builtin_clzll(x);
     }
 #endif
