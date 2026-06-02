c7e348f1d8e99bb353a1cf3ddbffd5e245f450f8
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-06-02 23:49:40 +0530

    I should not be lazy

--- board_learn/inference.py
+++ board_learn/inference.py
@@ -4,15 +4,25 @@ import torch
 
 import opening_model as om
 
-def inference(pos, form, checkpoint_dir="checkpoints"):
+def inference(pos, form, topk=5, checkpoint_dir="checkpoints"):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # how do i import num_classes?
+    # how do i import num_classes eco_classes?
+    eco_classes = []
     model = om.OpeningModel(n_classes=4000).to(device)
     model.load_state_dict(torch.load(os.path.join(checkpoint_dir, "final_model.pt")))
     model = torch.compile(model)
-    model.eval()
-    
     bb, sc = utils.inference_features(pos, form)
-    out = model(bb, sc)
-    return out
+    
+    with torch.no_grad():
+        out = model(bb, sc)
+    probs = torch.exp(out)
+    topk_probs, topk_idx = probs.topk(topk, dim=-1)
+   
+    for b in range(len(topk_idx)):
+        print("Top {topk} predicted openings with probabilities \n")
+        print("------------------------------------------------------------")
+        for idx, prob in zip(topk_idx[b].tolist(), topk_probs[b].tolist()):
+            print(f"{eco_classes[idx]} | {prob} \n")
+    
+    return topk_probs, topk_idx
