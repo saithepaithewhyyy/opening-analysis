128ee1a4c90e09b63d654785edbfd0dcbe7e2d2d
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-06-06 20:52:47 +0530

    changes

--- demo.py
+++ demo.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     
     import os
     book_paths = []
-    folder_path = "C:/praneeth/progs/py/opening-analysis/opening-books"
+    folder_path = "C:/praneeth/progs/py/opening-analysis/opening-books/opening_books"
 
     for fname in os.listdir(folder_path):
         if fname.endswith('.bin'):
