42a247e28bbbf7c89a447a11635f1f4631b83415
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-06-09 18:34:52 +0530

    speed up some things, do i need to really validate? we go brr brr

--- board_learn/train.py
+++ board_learn/train.py
@@ -15,8 +15,8 @@ from utils import sparse_kl_loss
 
 CHECKPOINTS_DIR = "checkpoints"
 TEST_SIZE = 0.1
-BATCH_SIZE = 256
-NUM_WORKERS = 4
+BATCH_SIZE = 512
+NUM_WORKERS = 2
 LR = 1e-3
 WEIGHT_DECAY = 1e-2
 CKPT_COUNT = 10
@@ -45,6 +45,7 @@ def train():
     device = torch.device('cuda' if torch.cuda.is_available()  # for cuda gpus
                           else 'mps' if torch.backends.mps.is_available() # for metal gpus (apple)
                           else 'cpu') # you dont have anything :(
+    use_amp = device.type in ("mps", "cuda")
     
     train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, pin_memory=True, collate_fn=opening_collate)
     test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, pin_memory=True, collate_fn=opening_collate)
@@ -60,7 +61,6 @@ def train():
     
     criterion = sparse_kl_loss
     optimizer = torch.optim.AdamW(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_batches)
     
     best_val = float('inf')
     total_loss = 0.0
@@ -69,62 +69,56 @@ def train():
         model.train()
         
         bb, sc, target = data
-        bb = bb.to(device)
-        sc = sc.to(device)
+        bb = bb.to(device, non_blocking=True)
+        sc = sc.to(device, non_blocking=True)
         
-        optimizer.zero_grad()
+        optimizer.zero_grad(set_to_none=True)
                     
-        out = model(bb, sc)
-        loss = criterion(out, target)
+        with torch.autocast(device_type=device.type, dtype=torch.float16, enabled=use_amp):
+            out = model(bb, sc)
+            loss = criterion(out, target)
         loss.backward() # type: ignore
-        lp = out
-        probs = lp.exp()
-        entropy = -(probs * lp).sum(dim=1)
         
-        grad_norm_unclipped = sum(p.grad.norm().item() ** 2 for p in model.parameters() if p.grad is not None) ** 0.5
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         
         optimizer.step()
         total_loss += loss.item()  # type: ignore 
-        scheduler.step()
         
-        ckpt_iter = max(1, len(train_loader) // CKPT_COUNT)
-        avg_val = None
-        if i%ckpt_iter == 0:
-            model.eval()
-            val_loss = 0
-            with torch.no_grad():
-                for bb_test, sc_test, target_test in test_loader:
-                    bb_test = bb_test.to(device)
-                    sc_test = sc_test.to(device)
-                    out_test = model(bb_test, sc_test)
-                    val_loss += criterion(out_test, target_test).item() # type: ignore
+        # ckpt_iter = max(1, len(train_loader) // CKPT_COUNT)
+        # avg_val = None
+        # if i%ckpt_iter == 0:
+        #     model.eval()
+        #     val_loss = 0
+        #     with torch.no_grad():
+        #         for bb_test, sc_test, target_test in test_loader:
+        #             bb_test = bb_test.to(device)
+        #             sc_test = sc_test.to(device)
+        #             out_test = model(bb_test, sc_test)
+        #             val_loss += criterion(out_test, target_test).item() # type: ignore
                     
-            avg_train = total_loss / (i+1)
-            avg_val = val_loss / len(test_loader)
+        #     avg_train = total_loss / (i+1)
+        #     avg_val = val_loss / len(test_loader)
                     
-            checkpoint = {
-                'epoch': i+1,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict(),
-                'train_loss': avg_train,
-                'val_loss': avg_val,
-                'eco_classes': eco_classes,
-            }
+        #     checkpoint = {
+        #         'epoch': i+1,
+        #         'model_state_dict': model.state_dict(),
+        #         'optimizer_state_dict': optimizer.state_dict(),
+        #         'scheduler_state_dict': scheduler.state_dict(),
+        #         'train_loss': avg_train,
+        #         'val_loss': avg_val,
+        #         'eco_classes': eco_classes,
+        #     }
             
-            ckpt_path = os.path.join(CHECKPOINTS_DIR, f"checkpoint_epoch{i+1:02d}.pt")
-            torch.save(checkpoint, ckpt_path)
+        #     ckpt_path = os.path.join(CHECKPOINTS_DIR, f"checkpoint_epoch{i+1:02d}.pt")
+        #     torch.save(checkpoint, ckpt_path)
             
-            if avg_val < best_val:
-                best_val = avg_val
-                ckpt_path = os.path.join(CHECKPOINTS_DIR, "final_model.pt")
-                torch.save(checkpoint, ckpt_path)
+        #     if avg_val < best_val:
+        #         best_val = avg_val
+        #         ckpt_path = os.path.join(CHECKPOINTS_DIR, "final_model.pt")
+        #         torch.save(checkpoint, ckpt_path)
 
-        val_info = f" | val_loss={avg_val:.4f}" if avg_val is not None else ""
-        tqdm.write(f"Step {i+1:02d} | train_loss={total_loss/(i+1):.4f} | "
-              f"gradient norm unclipped={grad_norm_unclipped:.4f} | "
-              f"entropy_mean={entropy.mean().item():.4f}{val_info}")
+        # val_info = f" | val_loss={avg_val:.4f}" if avg_val is not None else ""
+        tqdm.write(f"Step {i+1:02d} | train_loss={total_loss/(i+1):.4f} | ")
             
 
     model.eval()
@@ -147,7 +141,6 @@ def train():
             'epoch': len(train_loader)+1,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler_state_dict': scheduler.state_dict(),
             'train_loss': avg_train,
             'val_loss': avg_val,
             'eco_classes': eco_classes,
