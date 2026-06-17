67ac229743e11cf535e3da5c86f13cc3cda603b6
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-06-17 19:53:18 +0530

    get attn maps and gradients for visualisation at inference time

--- board_learn/inference.py
+++ board_learn/inference.py
@@ -4,11 +4,12 @@ import torch
 import pickle
 
 import opening_model as om
+import visualisation as viz
 
 CHECKPOINTS_DIR = "checkpoints"
 FINAL_MODEL_PATH = "final_model.pt"
 
-def inference(pos, form="fen", topk=5, checkpoint_dir=CHECKPOINTS_DIR):
+def inference(pos, form="fen", topk=5, visual_flag=True, checkpoint_dir=CHECKPOINTS_DIR):
     device = torch.device('cuda' if torch.cuda.is_available()
                         else 'mps' if torch.backends.mps.is_available()
                         else 'cpu')
@@ -21,11 +22,18 @@ def inference(pos, form="fen", topk=5, checkpoint_dir=CHECKPOINTS_DIR):
     model = torch.compile(model)
     model.load_state_dict(ckpt["model_state_dict"])
     bb, sc = utils.inference_features(pos, form)
+
     bb = torch.as_tensor(bb, dtype=torch.float32, device=device).unsqueeze(0)
     sc = torch.as_tensor(sc, dtype=torch.float32, device=device).unsqueeze(0)
+    attn_maps = {}
     
-    with torch.no_grad():
-        out = model(bb, sc)
+    if visual_flag:
+        bb = bb.requires_grad_(True)
+        out, attn_maps = model.forward_with_attn_maps(bb, sc)
+    else:
+        with torch.no_grad():
+            out = model(bb, sc)
+    
     probs = torch.exp(out)
     topk_probs, topk_idx = probs.topk(topk, dim=-1)
    
@@ -36,9 +44,12 @@ def inference(pos, form="fen", topk=5, checkpoint_dir=CHECKPOINTS_DIR):
         max_len = max(len(name) for name in eco_classes)
         for idx, prob in zip(topk_idx[b].tolist(), topk_probs[b].tolist()):
             print(f"{eco_classes[idx]:<{max_len}} | {prob}")
+
+    if visual_flag:
+        viz.visualize(attn_maps, bb.grad[0])
     
     return topk_probs, topk_idx
 
 if __name__ == "__main__":
     fen = "rnbqkbnr/pp1ppp1p/6p1/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 3"
-    inference(pos=fen, form="fen", topk=10)
+    inference(pos=fen, form="fen", topk=10, visual_flag=False)
