71778845ec4dfdabac8406b68db8bebe112dbec9
saithepaithewhyyy <97836271+saithepaithewhyyy@users.noreply.github.com>
2026-06-17 19:55:58 +0530

    move one line of code
    Enable gradient tracking for bounding box tensor.

--- board_learn/inference.py
+++ board_learn/inference.py
@@ -23,12 +23,11 @@ def inference(pos, form="fen", topk=5, visual_flag=True, checkpoint_dir=CHECKPOI
     model.load_state_dict(ckpt["model_state_dict"])
     bb, sc = utils.inference_features(pos, form)
 
-    bb = torch.as_tensor(bb, dtype=torch.float32, device=device).unsqueeze(0)
+    bb = torch.as_tensor(bb, dtype=torch.float32, device=device).unsqueeze(0).requires_grad_(True)
     sc = torch.as_tensor(sc, dtype=torch.float32, device=device).unsqueeze(0)
     attn_maps = {}
     
     if visual_flag:
-        bb = bb.requires_grad_(True)
         out, attn_maps = model.forward_with_attn_maps(bb, sc)
     else:
         with torch.no_grad():
